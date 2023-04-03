a387e9377ee834d6d08bac71bd7a68a7815d7d5c
maheshkumarprajapat <maheshprajapat258@gmail.com>
2023-04-03 11:02:40 +0530

    lazy loading use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Fragment, Suspense, lazy, useEffect, useState } from 'react'
-import { Banner, Collections, Featured, Offer, Products, SellingProducts, Service, Subscribe, Trending } from 'screens';
-import { imgFashion, imgLoose } from 'assets/images';
 import AOS from "aos";
+import { imgFashion, imgLoose } from 'assets/images';
 
 // Components
 const Header = lazy(() => import("components/header"));
 const Preloader = lazy(() => import("components/preloader"));
 const Footer = lazy(() => import("components/footer"));
+
+// Screens
+const Banner = lazy(() => import("screens/banner"));
+const Collections = lazy(() => import("screens/collections"));
+const Featured = lazy(() => import("screens/featured"));
+const Offer = lazy(() => import("screens/offer"));
+const Products = lazy(() => import("screens/products"));
+const SellingProducts = lazy(() => import("screens/selling-products"));
+const Service = lazy(() => import("screens/service"));
+const Subscribe = lazy(() => import("screens/subscribe"));
+const Trending = lazy(() => import("screens/trending"));
+
 
 const App = () => {
   let [loading, setLoading] = useState(false);
